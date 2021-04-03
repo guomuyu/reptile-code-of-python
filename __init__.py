51924906a7062db11c32e4d24ec0392a95b34766
GuoMuyu <81867471+guomuyu@users.noreply.github.com>
2021-04-03 23:12:27 +0800

    基金报告爬取__init__

---
+++ __init__.py
@@ -0,0 +1,17 @@
+# Date:     2021/4/3 16:33
+# Description: 爬取基金季度报告
+
+import pandas as pd
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
+
+# 显示所有列
+pd.set_option('display.max_columns', None)
+# 显示所有行
+# pd.set_option('display.max_rows', None)
+
+
+if __name__ == '__main__':
+    pass
